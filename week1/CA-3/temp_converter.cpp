2dcb198bd732540868be01bc225e4b8a27a93bb1
pedro <pedro.mc11@icloud.com>
2025-04-08 21:49:57 -0700

    Adds temperature converter program to Coding Assignment-3

---
+++ week1/CA-3/temp_converter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double temperature;
+    char unit;
+
+    
+    cout << "Temperature Converter" << endl;
+    cout << "*********************" << endl;
+
+    cout << "Enter temperature value: ";
+    cin >> temperature;
+
+    cout << "Enter unit (C for Celsius, F for Fahrenheit): ";
+    cin >> unit;
+
+    if(unit == 'C') {
+        cout << "\n" << temperature << "˚C is equal to " << (temperature * 9/5) + 32 << "˚F";
+    }
+    else {
+        cout << "\n" << temperature << "˚F is equal to " << (temperature - 32) * 5/9 << "˚C" << endl;
+    }
+    return 0;
+}
+
+// F = (C * 9/5) + 32
+// C = (F - 32) * 5/9
+
+// Temperature Converter
+// Enter temperature value: 100
+// Enter unit (C for Celsius, F for Fahrenheit): C
+// 100°C is equal to 212°F
